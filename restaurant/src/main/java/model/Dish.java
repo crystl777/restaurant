adda755b349cfe2b954d4ccfcd29d6f66a213fa9
Yakov Butenko <wx1324@yandex.ru>
2020-08-10 17:28:09 +0300

    0.2

--- restaurant/src/main/java/model/Dish.java
+++ restaurant/src/main/java/model/Dish.java
@@ -1,6 +1,41 @@
 package model;
 
-public class Dish {
+import java.time.LocalDate;
+
+public class Dish extends AbstractNamedEntity {
+
+    private int price;
+
+    private LocalDate localDate;
+
+    private Restaurant restaurant;
+
+    public Dish(int price, LocalDate localDate, Restaurant restaurant) {
+        this.price = price;
+        this.restaurant = restaurant;
+        this.localDate = localDate;
 
 
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
 }
