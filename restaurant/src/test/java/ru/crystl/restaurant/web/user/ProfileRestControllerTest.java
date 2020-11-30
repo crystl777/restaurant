141f21b7205d18ddbe624d7bfe833c742d19d9e4
Yakov Butenko <wx1324@yandex.ru>
2020-11-30 17:30:30 +0300

    8.1 started to create ProfileRestControllerTest

--- restaurant/src/test/java/ru/crystl/restaurant/web/user/ProfileRestControllerTest.java
+++ restaurant/src/test/java/ru/crystl/restaurant/web/user/ProfileRestControllerTest.java
@@ -1,4 +1,51 @@
 package ru.crystl.restaurant.web.user;
 
-public class ProfileRestControllerTest extends ProfileRestController {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.crystl.restaurant.to.UserTo;
+import ru.crystl.restaurant.web.AbstractControllerTest;
+import ru.crystl.restaurant.web.json.JsonUtil;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.crystl.restaurant.TestUtil.userHttpBasic;
+import static ru.crystl.restaurant.UserTestData.*;
+
+public class ProfileRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = ProfileRestController.REST_URL + "/";
+
+    @Autowired
+    private ProfileRestController controller;
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(USER_MATCHER.contentJson(USER));
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL))
+                .andExpect(status().isNoContent());
+        USER_MATCHER.assertMatch(controller.getAll(), ADMIN);
+    }
+
+    @Test
+    void update() throws Exception {
+        UserTo updated = new UserTo(null, "newName", "user@yandex.ru", "newPassword", 1500);
+        perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        USER_MATCHER.assertMatch(controller.get(USER_ID), updated);
+    }
+
 }
