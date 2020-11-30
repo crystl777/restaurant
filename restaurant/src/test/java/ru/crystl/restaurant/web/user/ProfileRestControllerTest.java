9f987c451b8e33d949d71f17a6027583da4bd6dd
Yakov Butenko <wx1324@yandex.ru>
2020-11-30 21:21:37 +0300

    8.2 create ProfileRestControllerTest

--- restaurant/src/test/java/ru/crystl/restaurant/web/user/ProfileRestControllerTest.java
+++ restaurant/src/test/java/ru/crystl/restaurant/web/user/ProfileRestControllerTest.java
@@ -3,16 +3,23 @@ package ru.crystl.restaurant.web.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.crystl.restaurant.model.User;
 import ru.crystl.restaurant.to.UserTo;
+import ru.crystl.restaurant.util.UserUtil;
 import ru.crystl.restaurant.web.AbstractControllerTest;
 import ru.crystl.restaurant.web.json.JsonUtil;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.crystl.restaurant.TestUtil.readFromJson;
 import static ru.crystl.restaurant.TestUtil.userHttpBasic;
 import static ru.crystl.restaurant.UserTestData.*;
+import static ru.crystl.restaurant.util.exception.ErrorType.VALIDATION_ERROR;
 
 public class ProfileRestControllerTest extends AbstractControllerTest {
 
@@ -31,21 +38,54 @@ public class ProfileRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+    @Test
     void delete() throws Exception {
-        perform(MockMvcRequestBuilders.delete(REST_URL))
+        perform(MockMvcRequestBuilders.delete(REST_URL)
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isNoContent());
         USER_MATCHER.assertMatch(controller.getAll(), ADMIN);
     }
-
+    @Test
+    void register() throws Exception {
+        UserTo newTo = new UserTo(null, "newName", "newemail@ya.ru", "newPassword");
+        User newUser = UserUtil.createNewFromTo(newTo);
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL + "/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newTo)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+        User created = readFromJson(action, User.class);
+        int newId = created.getId();
+        newUser.setId(newId);
+        USER_MATCHER.assertMatch(created, newUser);
+        USER_MATCHER.assertMatch(controller.get(newId), newUser);
+    }
     @Test
     void update() throws Exception {
-        UserTo updated = new UserTo(null, "newName", "user@yandex.ru", "newPassword", 1500);
+        UserTo updatedTo = new UserTo(null, "newName", "newMail@yandex.ru", "newPassword");
         perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(updated)))
+                .with(userHttpBasic(USER))
+                .content(JsonUtil.writeValue(updatedTo)))
                 .andDo(print())
                 .andExpect(status().isNoContent());
+        USER_MATCHER.assertMatch(controller.get(USER_ID), UserUtil.updateFromTo(new User(USER), updatedTo));
+    }
 
-        USER_MATCHER.assertMatch(controller.get(USER_ID), updated);
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    void updateDuplicate() throws Exception {
+        UserTo updatedTo = new UserTo(null, "newName", "admin@gmail.com", "newPassword");
+        perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(USER))
+                .content(JsonUtil.writeValue(updatedTo)))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(errorType(VALIDATION_ERROR))
+                .andDo(print());
     }
 
 }
